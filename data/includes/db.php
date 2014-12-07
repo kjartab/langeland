8f06d7077b94f74b9216423f9f4c915fdc7e0e11
Kjartan <kjartanbjorset@gmail.com>
2014-12-07 20:20:05 +0100

    siste

--- data/includes/db.php
+++ data/includes/db.php
@@ -1,7 +1,7 @@
 <?php
 
 $db = array(
-                'connectionString' => 'host=localhost port=5433 dbname=langeland user=postgres password=kjartan',
+                'connectionString' => 'host=localhost port=5432 dbname=langeland user=postgres password=kjartan1',
                 'host' => 'localhost',
                 'username' => 'postgres',
                 'password' => 'kjartan',
